b3ceeda9983084fa6de5fa1cd45bb35ebc4064ee
trung1007 <trungthanhcao.2003@gmail.com>
2024-11-05 09:33:22 +0700

    feat: demo detect message and show notification popup

--- App.tsx
+++ App.tsx
@@ -1,9 +1,31 @@
-import React, { useRef } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { SafeAreaView, View, Text, Button, StyleSheet } from 'react-native';
 import NotificationPopup from 'react-native-push-notification-popup';
+import useSmsPermission from './hook/useSmsPermision';
+import useVectorized from './hook/useVectorized';
 
 const App = () => {
+
+  const { receiveSmsPermission, receivedSmsMessage, receivedSmsPhoneNumber } =
+    useSmsPermission();
+    const {vectorizedDocument} = useVectorized(receivedSmsMessage);
+
+  const KNN = require('ml-knn');
+  const KnnModelSaved = require('./assets/model/savedKnnModel.json');
+  const knn_model = KNN.load(KnnModelSaved);
+
+  const handleDetectMessage=(message:any)=>{
+    if(message?.length>0){
+      const prediction = knn_model.predict(message)
+      if(prediction){
+        showNotification()
+      }
+    }
+  }
+
+
   const popupRef = useRef(null);
+  const [notification, setNotification] = useState(false)
 
   const showNotification = () => {
     if (popupRef.current) {
@@ -11,16 +33,22 @@ const App = () => {
       popupRef.current.show({
         appTitle: 'My App',
         timeText: 'Now',
-        title: 'New Message',
-        body: 'You have a new notification!',
+        title: 'Warning',
+        body: 'This message is spam',
       });
     }
   };
 
+  useEffect(()=>{
+    handleDetectMessage(vectorizedDocument)
+  },[vectorizedDocument])
+
   return (
     <SafeAreaView style={styles.container}>
       <View style={styles.container}>
-        <Button title="Show Notification" onPress={showNotification} />
+        <Text>Permission Status: {receiveSmsPermission}</Text>
+        <Text>Sender Phone Number: {receivedSmsPhoneNumber}</Text>
+        <Text>Received SMS: {receivedSmsMessage}</Text>
         <NotificationPopup
           ref={popupRef}
           renderPopupContent={({ appIconSource, appTitle, timeText, title, body }) => (
@@ -33,6 +61,7 @@ const App = () => {
           shouldChildHandleResponderMove={true}
           isSkipStatusBarPadding={true}
         />
+
       </View>
     </SafeAreaView>
   );
@@ -41,8 +70,8 @@ const App = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+    // justifyContent: 'center',
+    // alignItems: 'center',
     padding: 16,
   },
   popupContainer: {
